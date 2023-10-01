cb082a5dd85e4cc37f4334fcb07b4335a379ca3d
Marco <marcoqg_10@hotmail.com>
2023-10-01 14:02:06 -0500

    Pantalla Login
    Ya esta el 99% del front end de inicio de sesion

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Banner from './paginas/components/Banner';
 import PerfilJuez from './paginas/miPerfil/PerfilJuez';
 import DemandaJuez from './paginas/demandas/DemandaJuez';
 import DetalleDemanda from './paginas/detalleDemanda/DetalleDemanda';
+import Login from './paginas/inicioSesion/InicioSesion';
 
 function App() {
   return (
-    <div className='Principal'>
+   /* <div className='Principal'>
       <Banner/>
       <DetalleDemanda/>
-    </div>
+    </div>  
+    Para usar el login se tiene que hacer aparte
+    */
+
+    <Login/>
   );
 }
 
---
+++ src/paginas/inicioSesion/InicioSesion.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Logo from "../components/Logo";
+import "./InicioSesion.css";
+import logo from "../../imagenes/logo.png";
+
+export default function Login() {
+    return (
+        <div className="pantallalogin">
+            <div className="divsuperior">
+                <img src={logo} alt="Logo"  className="logo-image" /> 
+            </div>
+            <div className="divinferior">
+                <div className="contenidologin">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <h1>Iniciar Sesión</h1>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="Elemento">
+                                    <label >Usuario:</label>
+                                    <input className="caja" type="text" name="User" id="User" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="Elemento">
+                                    <label >Contraseña:</label>
+                                    <input className="caja" type="password" name="passw" id="passw" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td >
+                                    <input className="entrar" type="submit" value="Login" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="Elemento">
+                                    <p>¿No estás registrado? Regístrate aquí</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
