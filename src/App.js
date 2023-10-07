2a9a3046247bc33eb8e7bf14499a7c86d99c0fa8
Marco <marcoqg_10@hotmail.com>
2023-10-07 13:32:15 -0500

    Registro completo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Banner from './paginas/components/Banner';
+import "./App.css";
+import Banner from "./paginas/components/Banner";
 /* import Base from './paginas/Base'; //Esta es la pagina base, utilicenla para crear sus vistas */
-import PerfilJuez from './paginas/miPerfil/PerfilJuez';
-import DemandaJuez from './paginas/demandas/DemandaJuez';
-import DetalleDemanda from './paginas/detalleDemanda/DetalleDemanda';
-import Login from './paginas/inicioSesion/InicioSesion';
-import Registro from './paginas/registroUsuario/registro';
+import PerfilJuez from "./paginas/miPerfil/PerfilJuez";
+import DemandaJuez from "./paginas/demandas/DemandaJuez";
+import DetalleDemanda from "./paginas/detalleDemanda/DetalleDemanda";
+import Login from "./paginas/inicioSesion/InicioSesion";
+import Registro from "./paginas/registroUsuario/registro";
 
 function App() {
   return (
-   /* <div className='Principal'>
+    /* <div className='Principal'>
       <Banner/>
       <DetalleDemanda/>
     </div>  
     Para usar el login se tiene que hacer aparte
-    */ 
+    */
 
-    <Registro/>
+    <Registro />
   );
 }
 
--- src/paginas/registroUsuario/registro.js
+++ src/paginas/registroUsuario/registro.js
@@ -4,68 +4,54 @@ import "./registro.css";
 import logo from "../../imagenes/logo.png";
 
 export default function Registro() {
-    return (
-        <div className="pantallalogin">
-            <div className="divsuperior">
-                <img src={logo} alt="Logo"  className="logo-image" /> 
+  return (
+    <div className="pantallalogin">
+      <div className="divsuperior">
+        <img src={logo} alt="Logo" className="logo-image" />
+      </div>
+      <div className="divinferior">
+        <div className="contenidologin">
+       
+        <h1 className="titulo">Registro de usuario</h1>
+          
+          <div className="Elemento">
+            <label>Correo electrónico:</label>
+            <input className="caja" type="text" name="User" id="User" />
+          </div>
+          <div className="Elemento">
+          <label>Documento:</label>
+          <div className="documento-container">
+          
+              <select className="caja-documento">
+                <option value="DNI">DNI</option>
+                <option value="Pasaporte">Pasaporte</option>
+                <option value="Otro">Otro</option>
+              </select>
+              <input className="caja caja-segunda" type="text" name="doc" id="doc" />
             </div>
-            <div className="divinferior">
-                <div className="contenidologin">
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td className="Elemento" >
-                                    <h1>Registro de usuario</h1>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="Elemento">
-                                    <label >Correo electrónico:</label>
-                                    <input className="caja" type="text" name="User" id="User" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="Elemento">
-                                    <label >Documento:</label>
-                                    <input className="caja" type="text" name="doc" id="doc" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="Elemento">
-                                    <label >Nombre completo:</label>
-                                    <input className="caja" type="text" name="passw" id="passw" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="Elemento">
-                                    <label >Número de celular:</label>
-                                    <input className="caja" type="text" name="celu" id="celu" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="Elemento">
-                                    <label >Contraseña:</label>
-                                    <input className="caja" type="text" name="pass" id="pass" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="Elemento">
-                                    <label >Confirmar contraseña:</label>
-                                    <input className="caja" type="text" name="pass" id="pass" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input className="volver" type="submit" value="Volver" />
-                                </td>
-                                <td >
-                                    <input className="registrar" type="submit" value="Registrar" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+          </div>
+          <div className="Elemento">
+            <label>Nombre completo:</label>
+            <input className="caja" type="text" name="passw" id="passw" />
+          </div>
+          <div className="Elemento">
+            <label>Número de celular:</label>
+            <input className="caja" type="text" name="celu" id="celu" />
+          </div>
+          <div className="Elemento">
+            <label>Contraseña:</label>
+            <input className="caja" type="text" name="pass" id="pass" />
+          </div>
+          <div className="Elemento">
+            <label>Confirmar contraseña:</label>
+            <input className="caja" type="text" name="pass" id="pass" />
+          </div>
+          <div className="botones">
+            <input className="volver" type="submit" value="Volver" />
+            <input className="registrar" type="submit" value="Registrar" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
