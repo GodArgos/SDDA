d6b6d6c526bbf9733e2a778bbea912e006be0f6f
Your Name <meixinliu1997@gmail.com>
2023-10-12 11:21:06 -0500

    rip abogado

--- src/paginas/pagJuez/pagDetalleDemanda/infoPersona/BoxInfDemandado.js
+++ src/paginas/pagJuez/pagDetalleDemanda/infoPersona/BoxInfDemandado.js
@@ -1,4 +1,3 @@
-import InfoInfAbogado from "./InfoInfAbogado";
 import InfoInfPersona from "./InfoInfPersona";
 import BotonVerExpediente from "../../../../components/BotonVerExpediente";
 import "./BoxInfPersona.css";
@@ -8,15 +7,7 @@ export default function BoxInfDemandado() {
         <div id="InfPersona">
             <div className="Box">
                 <h3>Información personal del demandado: </h3>
-                <InfoInfAbogado
-                    nombres="Mike owo"
-                    apellidos="Guillen"
-                    dni="78964512"
-                    direccion="nose mano ayuda"
-                    sexo="macho pecho peludo"
-                    colegiatura="789654"
-                />
-                <br />
+                
                 INFORMACIÓN DEL DEMANDADO:
                 <InfoInfPersona
                     nombres="ayuda"
--- src/paginas/pagJuez/pagDetalleDemanda/infoPersona/BoxInfDemandante.js
+++ src/paginas/pagJuez/pagDetalleDemanda/infoPersona/BoxInfDemandante.js
@@ -1,4 +1,3 @@
-import InfoInfAbogado from "./InfoInfAbogado";
 import InfoInfPersona from "./InfoInfPersona";
 import BotonVerExpediente from "../../../../components/BotonVerExpediente";
 import "./BoxInfPersona.css";
@@ -8,15 +7,7 @@ export default function BoxInfDemandate() {
         <div id="InfPersona">
             <div className="Box">
                 <h3>Información personal del demandante: </h3>
-                <InfoInfAbogado
-                    nombres="juan owo"
-                    apellidos="lion sea"
-                    dni="78964512"
-                    direccion="ayuda"
-                    sexo="non"
-                    colegiatura="789654"
-                />
-                <br />
+                
                 INFORMACIÓN DEL DEMANDANTE:
                 <InfoInfPersona
                     nombres="owo"
