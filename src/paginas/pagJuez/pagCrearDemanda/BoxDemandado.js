f90dca1cb05e793e1cbbd46a0ef8c08037e194a9
Marco <marcoqg_10@hotmail.com>
2023-10-14 17:18:08 -0500

    crear demanda
    botones de demandado y demandante

--- src/paginas/pagJuez/pagCrearDemanda/BoxDemandado.js
+++ src/paginas/pagJuez/pagCrearDemanda/BoxDemandado.js
@@ -3,7 +3,13 @@ import "./CrearDemanda.css"
 
 const BoxDemandado = () => {
   return (
+    <div className='C'>
     <div className="right-box">Demandado Datos</div>
+    <div>
+        <button className="reject-button">Rechazar</button>
+        <button className="accept-button">Aceptar</button>
+      </div>
+    </div>
   );
 }
 
