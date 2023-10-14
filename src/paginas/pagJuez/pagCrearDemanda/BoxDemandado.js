15894472f1875b881ee0ff0f2cc13358629896ff
Marco <marcoqg_10@hotmail.com>
2023-10-14 18:21:12 -0500

    Crear Demanda Listo

--- src/paginas/pagJuez/pagCrearDemanda/BoxDemandado.js
+++ src/paginas/pagJuez/pagCrearDemanda/BoxDemandado.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import "./CrearDemanda.css"
+import React from "react";
+import "./CrearDemanda.css";
 
 const BoxDemandado = () => {
   return (
-    <div className='C'>
-    <div className="right-box">Demandado Datos</div>
-    <div>
+    <div className="C">
+      <div className="right-box">
+        Demandado Datos
+        <div className="form-container">
+        <form>
+          <div className="form-group">
+            <label htmlFor="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" className="rounded-input" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="apellidos">Apellidos:</label>
+            <input type="text" id="apellidos" name="apellidos" className="rounded-input" />
+          </div>
+          <div className="form-group">
+            <label>Género:</label>
+            <div className="radio-options">
+              <input type="radio" id="genero-masculino" name="genero" value="masculino" />
+              <label htmlFor="genero-masculino">Masculino</label>
+              <input type="radio" id="genero-femenino" name="genero" value="femenino" />
+              <label htmlFor="genero-femenino">Femenino</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="dni">DNI:</label>
+            <input type="text" id="dni" name="dni" className="rounded-input" />
+          </div>
+        </form>
+      </div>
+      </div>
+      <div>
         <button className="reject-button">Rechazar</button>
         <button className="accept-button">Aceptar</button>
       </div>
     </div>
   );
-}
+};
 
 export default BoxDemandado;
--- src/paginas/pagJuez/pagCrearDemanda/BoxDemandante.js
+++ src/paginas/pagJuez/pagCrearDemanda/BoxDemandante.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import "./CrearDemanda.css"
-import InfoDemandante from './InfoDemandante';
+import React from "react";
+import "./CrearDemanda.css";
+import InfoDemandante from "./InfoDemandante";
 
 const BoxDemandante = () => {
   return (
-    <div className='C'>
-    <div className="left-box">Demandante Datos
-
-    <div className="CajaDemandante" >
-            <div className="BoxDemandanteText">
-                <InfoDemandante
-                    nombre="Rasec"
-                    apellidos="Rafael"
-                    genero="si ,por favor"
-                    dni="69696"
-                />
-            </div>
+    <div className="C">
+      <div className="left-box">
+        Demandante Datos
+        <div className="CajaDemandante">
+          <div className="BoxDemandanteText">
+            <InfoDemandante
+              nombre="Rasec"
+              apellidos="Rafael"
+              genero="si ,por favor"
+              dni="69696"
+            />
+          </div>
         </div>
-        
-    </div>
-    <button className="download-button">Descargar PDF</button>
+      </div>
+      <button className="download-button">Descargar PDF</button>
     </div>
   );
-}
+};
 
 export default BoxDemandante;
