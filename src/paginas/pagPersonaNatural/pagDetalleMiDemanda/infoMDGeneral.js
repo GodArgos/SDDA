61574c879d24de54ba2e3c8c97c0d89dbfa999b8
Marco <marcoqg_10@hotmail.com>
2023-11-27 23:03:05 -0500

    comentario rechazo 2.0

--- src/paginas/pagPersonaNatural/pagDetalleMiDemanda/infoMDGeneral.js
+++ src/paginas/pagPersonaNatural/pagDetalleMiDemanda/infoMDGeneral.js
@@ -1,7 +1,6 @@
 export default function InfoMDGeneral(props){
     const infoDemanda = props.infoDemanda;
     const estadosDemanda = ["Recibida", "En Proceso", "Finalizada", "Rechazada"];
-    console.log(infoDemanda)
 
     function generarTextoEstado(valor){
         return(estadosDemanda[valor - 1]);
@@ -12,7 +11,7 @@ export default function InfoMDGeneral(props){
             <div className="BTexto" id="BInfoDemanda">
                 <p><b>Descripción:</b> {infoDemanda.descripcion}</p>
                 <p><b>Fecha de emisión:</b> {infoDemanda.fecha_emision}</p>
-                {infoDemanda.fecha_audiencia && (
+                {infoDemanda.estadoDemandaId !== 4 && infoDemanda.fecha_audiencia && (
                     <p><b>Fecha de Audiencia:</b> {infoDemanda.fecha_audiencia}</p>
                 )}
                 <p><b>Estado de Demanda:</b> {generarTextoEstado(infoDemanda.estadoDemandaId)}</p>
